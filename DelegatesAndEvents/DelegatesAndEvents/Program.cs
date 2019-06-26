3a9e8053e35e760ec7cda2acbcf092ae6c36ceb8
Jack Xia <Jack.Xia@devfacto.com>
2019-06-26 11:13:38 -0600

    init

---
+++ DelegatesAndEvents/DelegatesAndEvents/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace DelegatesAndEvents
+{
+    public delegate void WorkPerformedHandler(int hours, WorkType workType);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WorkPerformedHandler del1 = new WorkPerformedHandler(WorkPerformed1);
+            WorkPerformedHandler del2 = new WorkPerformedHandler(WorkPerformed2);
+
+            del1(5, WorkType.Golf);
+            DoWork(del1);
+            del2(10, WorkType.GenerateReports);
+            
+        }
+
+        static void DoWork(WorkPerformedHandler del)
+        {
+            del(5, WorkType.GoToMeetings);
+        }
+        
+        
+
+        static void WorkPerformed1(int hours, WorkType workType)
+        {
+            Console.WriteLine("WorkPerformed1 called " +hours.ToString() );
+        }
+
+        static void WorkPerformed2(int hours, WorkType workType)
+        {
+            Console.WriteLine("WorkPerformed2 called " + hours.ToString());
+        }
+    }
+
+
+    public enum WorkType
+    {
+        GoToMeetings,
+        Golf,
+        GenerateReports
+    }
+}
